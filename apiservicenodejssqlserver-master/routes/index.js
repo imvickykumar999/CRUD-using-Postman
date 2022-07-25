d09e99d665b477c23f08f132e05aab7b17f9b466
Vicky Kumar <imvickykumar999@gmail.com>
2022-07-25 16:32:35 +0530

    deepak dey's code

---
+++ apiservicenodejssqlserver-master/routes/index.js
@@ -0,0 +1,26 @@
+const { json } = require('express');
+var express = require('express');
+var router = express.Router();
+const sql = require("../dboperation");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+//test connection
+router.get('/testconnect', function(req, res, next) {
+  sql.getdata();
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get("/getdata_withQuery", function (req, res, next) {
+  sql.getdata_withQuery().then((result) => {
+    res.json(result[0]);
+  });
+});
+
+
+module.exports = router;
