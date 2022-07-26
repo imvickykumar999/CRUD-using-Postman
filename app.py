f68395ce62e7baac1a5e27a58e2b4e521fa1d88c
Vicky Kumar <imvickykumar999@gmail.com>
2022-07-26 14:33:18 +0530

    postman

--- app.py
+++ app.py
@@ -1,15 +1,62 @@
 
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
+from fetch import fire
+
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET', 'POST'])
-def home():
-	if(request.method == 'GET'):
+@app.route('/createuser', methods=['GET', 'POST'])
+def create_user():
+	content_type = request.headers.get('Content-Type')
+	if content_type == 'application/json':
+		content = request.json
 
-		from fetch import fire
-		path = 'apigee'
-		json_data = fire.call(path)
+		count = fire.call('counter')
+		fire.send('counter', count+1)
+
+		uid = content['unique_ID'] = count+1
+		_path = f"apigee/{uid}"
+
+		fire.send(_path, content)
+		return jsonify(content)
+
+
+@app.route('/getallusers')
+def get_all_users():
+	data = fire.call('apigee')
+	return jsonify(data)
+
+
+@app.route('/getuserbyid/<uid>')
+def get_user_by_id(uid):
+	json_data = fire.call(f'apigee/{uid}')
+
+	if json_data == None:
+		return jsonify({
+			'error' : {
+				'description' : f'Unique ID {uid} not Found in Database.'
+			}
+		})
+	else:
 		return jsonify(json_data)
+
+@app.route('/deleteuserbyid/<uid>', methods=['GET', 'POST', 'DELETE'])
+def delete_user_by_id(uid):
+	_path = f"apigee/{uid}"
+
+	if fire.call(_path) == None:
+		return jsonify({
+			'message' : {
+				'description' : f'Unique ID {uid} not fount in Database.'
+			}
+		})
+	else:
+		fire.send(_path, {})
+		return jsonify({
+				'message' : {
+					'description' : f'Unique ID {uid} has been deleted from Database.'
+				}
+			})
+
 
 if __name__ == '__main__':
 	app.run(debug = True)
--- fetch/fire.py
+++ fetch/fire.py
@@ -3,14 +3,22 @@ from firebase_admin import credentials
 import firebase_admin
 from firebase_admin import db
 
+
 cred = credentials.Certificate('fetch/cred.json')
 url = 'https://neosalpha-999-default-rtdb.firebaseio.com/'
 path = {'databaseURL' : url}
 
+
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred, path)
+
 
 def call(path = 'apigee'):
     refv = db.reference(path)
     name = refv.get()
     return name
+
+
+def send(path, data = {}):
+    refv = db.reference(path)
+    refv.set(data)
