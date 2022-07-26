840cc8e94372ded15d5f1522821f4ae032120c9f
Vicky Kumar <imvickykumar999@gmail.com>
2022-07-26 17:10:39 +0530

    encrypted

--- fetch/fire.py
+++ fetch/fire.py
@@ -2,7 +2,7 @@
 from firebase_admin import credentials
 import firebase_admin
 from firebase_admin import db
-
+import base64
 
 cred = credentials.Certificate('fetch/cred.json')
 url = 'https://neosalpha-999-default-rtdb.firebaseio.com/'
@@ -22,3 +22,19 @@ def call(path = 'apigee'):
 def send(path, data = {}):
     refv = db.reference(path)
     refv.set(data)
+
+
+def encodeit(message):
+    message_bytes = message.encode('ascii')
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_message = base64_bytes.decode('ascii')
+    return base64_message
+
+
+def decodeit(base64_message):
+    base64_bytes = base64_message.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    message = message_bytes.decode('ascii')
+    return message
+
+    
